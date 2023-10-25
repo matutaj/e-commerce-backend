6471fef9b8fb308e3d1c263a1f2ad0aade5f5592
matutaj <matutajorge2002@gmail.com>
2023-10-25 17:57:17 +0100

    feat: configure the struture of app

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 const port = process.env.PORT || 3333;
-
+import { routes } from "./routes";
 const app = express();
 
 
 app.use(express.json());
+
+app.use(routes)
 
 app.listen(Number(port), () => {
     console.log("Server Running...");

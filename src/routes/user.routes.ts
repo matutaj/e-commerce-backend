3165d17a7233de5addb2fe3311e54aa06a12ee2b
matutaj <matutajorge2002@gmail.com>
2023-10-29 09:31:49 +0100

    feat: add user router

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 
 import { CreateUserController } from "../models/user/useCase/create/CreateUserController";
+import { GetUserAllController } from "../models/user/useCase/getAllUser/GetUserAllController";
+
 
 const createUser = new CreateUserController()
-
+const getUserAll = new GetUserAllController()
 
 const userRoutes = Router()
 
 userRoutes.post("/", createUser.handle)
+userRoutes.get("/", getUserAll.handle)
 
 export { userRoutes }

ce9356785e352b997a8585775d64a16ded317562
matutaj <matutajorge2002@gmail.com>
2023-10-30 15:11:56 +0100

    feat: add get all produt routes

--- src/routes/produt.routes.ts
+++ src/routes/produt.routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 
 import { CreateProdutController } from "../models/produt/useCase/create/CreateProdutController";
-
+import { GetAllProdutController } from "../models/produt/useCase/getAll/GetAllProdutController";
 
 const createProdut = new CreateProdutController();
+const getAllProdut = new GetAllProdutController();
 
 
 const produtroutes = Router();
 
 produtroutes.post("/", createProdut.handle)
+produtroutes.get("/", getAllProdut.handle)
 
 export { produtroutes }

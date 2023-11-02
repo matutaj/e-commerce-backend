c969dca60e25324fd6d8ca8e97d7ad3999233220
matutaj <matutajorge2002@gmail.com>
2023-11-02 01:47:56 +0100

    feat: create carry interface

---
+++ src/models/carry/repositories/ICarry.ts
@@ -0,0 +1,17 @@
+import { Carry, Produt } from "@prisma/client"
+
+
+export interface DataCarry {
+    id?: string
+    produtId: string
+    userId: string
+}
+
+export interface ICarry {
+    create({ }: DataCarry): Promise<Carry>
+    getAll(): Promise<Carry[]>
+    getById(id: string): Promise<Carry | undefined>
+    getByName(name: string): Promise<Produt | undefined>
+    delete(id: string): Promise<void>
+    update({ }: DataCarry): Promise<Carry>
+}

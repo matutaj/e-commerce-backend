84e06f013240a98d864eb749393e52953260d0f0
matutaj <matutajorge2002@gmail.com>
2023-12-22 16:34:00 +0100

    add

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -9,18 +9,16 @@ import { categoryRoutes } from "./category.routes";
 import { carryRoutes } from "./carry.routes";
 import { buyRoutes } from "./buy.routes";
 
-
 const routes = Router();
 
 // routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc))
 
-routes.use("/contactType", typeContactRoutes)
-routes.use("/user", userRoutes)
-routes.use("/login", loginRoutes)
-routes.use("/category", categoryRoutes)
-routes.use("/carry", carryRoutes)
-routes.use("/produt", produtroutes)
-routes.use("/buy", buyRoutes
-)
+routes.use("/contactType", typeContactRoutes);
+routes.use("/user", userRoutes);
+routes.use("/login", loginRoutes);
+routes.use("/category", categoryRoutes);
+routes.use("/carry", carryRoutes);
+routes.use("/produt", produtroutes);
+routes.use("/buy", buyRoutes);
 
-export { routes }
+export { routes };
